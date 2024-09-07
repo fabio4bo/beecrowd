ec92482546de81d33d16a17e99a4e2f22150e995
Antonio Fábio <zantoniofabio@gmail.com>
2024-09-06 21:29:40 -0500

    Resolvidos todos os Tentados

---
+++ 2_ad-hoc/2191/2191.py
@@ -0,0 +1,41 @@
+"""
+BEE 2191 - Saldo de Gols - Nível 8 - Ad-hoc
+"""
+
+testes = 0
+while True:
+    try:
+        n = int(input())
+
+        if n == 0:
+            break
+
+        testes += 1
+        s_maior = -1
+        s_atual = 0
+        primeiro = 0
+        temp_primeiro = 0
+        ultimo = 0
+
+        for i in range(n):
+            x, y = [int(i) for i in input().strip().split(" ")]
+            s = x - y
+            s_atual += s
+
+            if s_atual < 0:
+                s_atual = 0
+                temp_primeiro = i + 1
+            if s_atual >= s_maior:
+                s_maior = s_atual
+                primeiro = temp_primeiro
+                ultimo = i
+
+        print(f"Teste {testes}")
+        if s_maior > 0:
+            print(f"{primeiro+1} {ultimo+1}")
+            print()
+        else:
+            print("nenhum")
+            print()
+    except EOFError:  # eof
+        break
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 
 | Categoria | Python | Java | C/C++ |
 | -- | -- | -- | -- |
-| Iniciante | 9 | 9 | 4 |
-| Ad-Hoc | 1 | 1 | 1 |
+| Iniciante | 9 | 9 | 5 |
+| Ad-Hoc | 3 | 1 | 3 |
 | Strings | 2 | 2 | 0 |
 | Estruturas e Bibliotecas | 1 | 0 | 0 |
-| Matemática | 16 | 2 | 1 |
+| Matemática | 17 | 2 | 1 |
 | Paradigmas | 0 | 0 | 0 |
 | Grafos | 0 | 0 | 0 |
 | Geometria Computacional | 0 | 0 | 0 |
 | SQL | 0 | 0 | 0 |
-| Total | 29 | 14 | 6 |
+| Total | 32 | 14 | 9 |
