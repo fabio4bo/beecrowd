dc89841cde7520c04e06c285a10fb7b802c5777f
Antonio Fábio <zodiecko@gmail.com>
2024-09-08 13:43:16 -0500

    Beginner: 5 solutions

---
+++ beginner/1001/1001.py
@@ -0,0 +1,9 @@
+"""
+BEE 1001 - Extremely Basic - Level 4 - Beginner
+"""
+
+a = int(input())
+b = int(input())
+
+x = a + b
+print(f"X = {x}")
---
+++ beginner/1002/1002.py
@@ -0,0 +1,10 @@
+"""
+BEE 1002 - Area of a Circle - Level 4 - Beginner
+"""
+
+N = 3.14159
+r = float(input())
+
+a = N * pow(r, 2.0)
+
+print(f"A={a:.4f}")
